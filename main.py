259cfd6527c556ec329da41afec3da7ec3b8c986
Henrique <mithisu001@gmail.com>
2025-05-09 21:52:25 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+running = True
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
